7631f9077618db37e872d84bf3f2cd579f431c4b
Harsh Benahalkar <benahalkar@gmail.com>
2022-01-16 14:17:50 +0530

    adding files

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 function random_rgb(){ 
-    let rgb = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
+    let rgb = "4px solid rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
     // console.log(typeof(rgb), rgb);
     return rgb;
 }
 
-// setInterval(
-//     function(){
-//         document.body.style.background = random_rgb()
-//     },1000
-// );
+setInterval(
+    function(){
+        let rgb = random_rgb();
+        document.getElementById("bubble").style.border = rgb;
+        document.getElementById("profile_pic").style.border = rgb;
+        document.getElementById("button_1").style.borderTop = rgb;
+        document.getElementById("button_1").style.borderBottom = rgb;
+        document.getElementById("button_2").style.borderTop = rgb;
+        document.getElementById("button_2").style.borderBottom = rgb;
+    },500
+);
